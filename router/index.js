22e57f4d9f4080c67516ff44fa6bafdfc92185d6
熊熊熊子路 <1711279251@qq.com>
2022-01-01 21:20:15 +0800

    重启项目

--- router/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-const router = new Router();
-
-const home = require('./home');
-
-// 启动 home
-router.use(home.routes(),home.allowedMethods());
-
-module.exports = router;
