510d2ef93875c25d8ee1c0b638a66298185442de
熊熊熊子路 <1711279251@qq.com>
2022-01-05 23:59:35 +0800

    feat：将中间件进行组合保护已实现的接口

--- src/router/back.router.js
+++ src/router/back.router.js
@@ -2,6 +2,7 @@
 const Router = require('koa-router');
 
 const loginMiddleware = require('../middleware/back/login.middleware')
+const tokenMiddleWare = require('../middleware/back/token.middleware')
 
 const loginController = require('../controller/back/login.controller')
 const accountController = require('../controller/back/account.controller')
@@ -13,8 +14,8 @@ const backRouter = new Router({
 // 登录接口
 backRouter.post('/login', loginMiddleware.checkAccount, loginMiddleware.checkPassword, loginController.login);
 // 获取全部数据
-backRouter.get('/accounts', accountController.getAllAccount);
+backRouter.get('/accounts', tokenMiddleWare.checkToken, accountController.getAllAccount);
 // 添加账号
-backRouter.post('/account/add', accountController.addAccount);
+backRouter.post('/account/add', tokenMiddleWare.checkToken,loginMiddleware.checkAccount, loginMiddleware.checkPassword, accountController.addAccount);
 
 module.exports = backRouter;
