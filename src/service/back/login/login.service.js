90abd6d2f317105aa19d25e2a5bdd20c48dcf9d2
熊熊熊子路 <1711279251@qq.com>
2022-05-13 00:42:33 +0800

    feat: 更新登陆返回的数据格式

--- src/service/back/login/login.service.js
+++ src/service/back/login/login.service.js
@@ -4,7 +4,17 @@ const adminPool = require('../../../db/adminPool')
 
 class LoginService{
     async login(account){
-        const sql = `select * from admin_users where account = '${account}';`
+        const sql = `select au.id       as id,
+                            au.account  as account,
+                            au.password as password,
+                            ar.id       as roleId,
+                            ar.name     as roleName
+                       from admin_users as au,
+                            admin_user_role as aur,
+                            admin_roles as ar
+                      where au.account = '${ account }'
+                        and au.id = aur.user_id
+                        and aur.role_id = ar.id;`;
         const res = await adminPool.execute(sql);
         return res[0][0];
     }
